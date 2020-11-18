62855f412955e4917fe7afa22c57f7434b8442a9
sameerapriya <sameerakorivi@yahoo.com>
2020-11-18 20:29:12 +0530

    saving rotated image into db

--- images/views.py
+++ images/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
-
+from PIL import Image
 from .forms import ImageTagForm
 from .models import ImageTag
 
@@ -37,9 +37,24 @@ def album_view(request):
 
 def image_id(request,id):
     """ Method for particular Image """
-    image = get_object_or_404(ImageTag,id=id)
+    imagetag = get_object_or_404(ImageTag,id=id)
     if request.method == 'GET':
-        return render(request,"images/image.html",{'imagetag':image})
+        return render(request,"images/image.html",{'imagetag':imagetag})
+    
+    if request.POST.get('clockwise', False):
+        image = imagetag.picture
+        imagepil = Image.open(image)
+        imagepil = imagepil.rotate(-90,expand=True) # Rotating the picture
+        imagepil.save(imagetag.picture.file.name,overwrite=True) # overwriting the saved picture 
+        return HttpResponse('Rotated Image successfully saved ')
+
+    if request.POST.get('anticlockwise', False):
+        image = imagetag.picture
+        imagepil = Image.open(image)
+        imagepil = imagepil.rotate(90,expand=True)
+        imagepil.save(imagetag.picture.file.name,overwrite=True)
+        return HttpResponse('Rotated Image successfully saved(AntiClockwise)')
+
 
 def index(request):
     """ Method for homepage """
